437d84cc42d4bc4ce717e4be955a6c15196762e2
alexandre ximenes <xyymenes@gmail.com>
2018-04-11 15:41:33 -0300

    prosseguindo com Comparator

--- Treinamento Java 8/src/OrdenandoStrings/OrdenaStrings.java
+++ Treinamento Java 8/src/OrdenandoStrings/OrdenaStrings.java
@@ -10,16 +10,40 @@ public class OrdenaStrings {
 	
 	public static void main(String[] args) {
 		
-		List<String> asList = Arrays.asList("string seis", "string um", "string dois", "string tres", "string quatro", "string cinco");
+		/**
+		 * [
+				 A ciência de hoje é a tecnologia de amanhã [Edward Teller], 
+				 Trocava toda minha tecnologia por uma tarde com Sócrates.[Steve Jobs], 
+				 A tecnologia só é tecnologia para quem nasceu antes dela ter sido inventada. [Alan Kay], 
+				 Tornou-se chocantemente óbvio que a nossa tecnologia excedeu a nossa humanidade. [Albert Einstein], 
+				 A maior invenção do mundo não é a minha tecnologia! É a morte! pois através dela, o velho sempre dará lugar para o novo! [Steve Jobs], 
+				 A primeira regra de qualquer tecnologia utilizada nos negócios é que a automação aplicada a uma operação eficiente aumentará a eficiência. 
+				 A segunda é que a automação aplicada a uma operação ineficiente aumentará a ineficiência. [Bill Gates]
+		   ]
+		 */
+		
+		List<String> palavras = Arrays.asList(
+				"\n Tornou-se chocantemente óbvio que a nossa tecnologia excedeu a nossa humanidade. [Albert Einstein]", 
+				
+				 "\n Trocava toda minha tecnologia por uma tarde com Sócrates.[Steve Jobs]",
+				 
+				 "\n A tecnologia só é tecnologia para quem nasceu antes dela ter sido inventada. [Alan Kay]",
+				 
+				 "\n A primeira regra de qualquer tecnologia utilizada nos negócios é que a automação aplicada a uma operação eficiente aumentará a eficiência. \n A segunda é que a automação aplicada a uma operação ineficiente aumentará a ineficiência. [Bill Gates]",
+				 
+				 "\n A maior invenção do mundo não é a minha tecnologia! É a morte! pois através dela, o velho sempre dará lugar para o novo! [Steve Jobs]",
+				 
+				 "\n A ciência de hoje é a tecnologia de amanhã [Edward Teller]");
+		
 		
 		/**
 		 * Ordena lexicograficamente
 		 * esperado [string cinco, string dois, string quatro, string seis, string tres, string um]
 		 */ 
 		Comparator<String> comparador = new ComparadorPorTamanho();
-		Collections.sort(asList, comparador);
+		palavras.sort(comparador);
 		
-		System.out.println(asList);
+		System.out.println(palavras);
 	}
 
 }

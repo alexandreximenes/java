32da6594fe2daecfea07dbf0d7284c0dd8325392
Alexandre Ximenes <xyymenes@gmail.com>
2018-04-22 13:28:48 -0300

    Cadastrando empresa com doPost

---
+++ Treinamento_Servlet/gerenciador/src/main/java/br/com/alexandre/gerenciador/web/CadastrarEmpresa.java
@@ -0,0 +1,30 @@
+package br.com.alexandre.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alexandre.gerenciador.Empresa;
+import br.com.alexandre.gerenciador.dao.EmpresaDAO;
+
+@WebServlet(urlPatterns = "/nova-empresa")
+public class CadastrarEmpresa extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String nome = req.getParameter("empresa");
+		Empresa empresa = new Empresa(nome);
+		new EmpresaDAO().adiciona(empresa);
+		PrintWriter writer = resp.getWriter();
+		writer.println(nome + " cadastrada com sucesso");
+	}
+}

ce42ab765276201af70e1240ac48194b0390dc21
Alexandre Ximenes <xyymenes@gmail.com>
2018-04-24 00:11:28 -0300

    refatorando Servlet Login

--- Treinamento_Servlet/gerenciador/src/main/java/br/com/alexandre/gerenciador/web/Login.java
+++ Treinamento_Servlet/gerenciador/src/main/java/br/com/alexandre/gerenciador/web/Login.java
@@ -14,28 +14,39 @@ import br.com.alexandre.gerenciador.Usuario;
 import br.com.alexandre.gerenciador.dao.UsuarioDAO;
 
 @WebServlet(urlPatterns = "/login")
-public class Login extends HttpServlet{
+public class Login extends HttpServlet {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		pagina /login é acessado via post, caso acesse via get é causado erro
+//		neste caso, esta direcionando para ela mesmo, caso alguem tente acessar via get
+		req.getRequestDispatcher("index.html").forward(req, resp);
+	}
+
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String email = req.getParameter("email");
 		String senha = req.getParameter("senha");
-		
+
 		Usuario usuario = new UsuarioDAO().buscaPorEmailESenha(email, senha);
+		getCookie(resp, email, usuario);
+
+	}
+
+	private void getCookie(HttpServletResponse resp, String email, Usuario usuario) throws IOException {
 		PrintWriter writer = resp.getWriter();
-		if(usuario != null) {
+		if (usuario != null) {
 			Cookie cookie = new Cookie("usuario.logado", email);
-			cookie.setMaxAge(100);
+			cookie.setMaxAge(60*10);
+			//cookie.setSecure(true);
 			resp.addCookie(cookie);
-			writer.println("<html><body> " +email +" logado com sucesso <body></html>");
-		}else {
-			writer.println("<html><body> usuario invalido! <body></html>");
+			writer.println("<html><body> " + email + " logado com sucesso </body></html>");
+		} else {
+			writer.println("<html><body> usuario invalido! </body></html>");
 		}
-		
-		
 	}
 }

af9238f1ce44fccc6bf6f8fc83443e6dce8a9ccc
Alexandre Ximenes <xyymenes@gmail.com>
2018-05-17 16:42:50 -0300

    Usando Injecao de Dependencia com @Repository @Autowired e dataSource
    Configurado bean da apache commons com as propriedades do banco de dados, injetei as dependencias da conta dao no controller ContaController, e recebido no contrutor da contaDAO o datasource

--- Treinamento_Spring/1 Spring MVC/projeto-spring/src/br/com/alexandre/contas/controller/ContaController.java
+++ Treinamento_Spring/1 Spring MVC/projeto-spring/src/br/com/alexandre/contas/controller/ContaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,7 +15,14 @@ import br.com.alexandre.contas.dao.ContaDAO;
 import br.com.alexandre.contas.modelo.Conta;
 
 @Controller
-public class FormularioContaController {
+public class ContaController {
+	
+	private ContaDAO dao;
+
+	@Autowired
+	public ContaController(ContaDAO dao) {
+		this.dao = dao;
+	}
 
 	@RequestMapping("/formulario")
 	public String formulario() {
@@ -31,7 +39,7 @@ public class FormularioContaController {
 		
 		System.out.println(conta.getDescricao() + " adiciona com sucesso");
 		
-		new ContaDAO().adiciona(conta);
+		dao.adiciona(conta);
 		
 		System.out.println(conta);
 		
@@ -42,7 +50,7 @@ public class FormularioContaController {
 	@RequestMapping("pagaConta")
 	public String pagar(Long id, HttpServletResponse response) {
 		
-		new ContaDAO().paga(id);
+		dao.paga(id);
 		
 		response.setStatus(200);
 		
@@ -52,7 +60,7 @@ public class FormularioContaController {
 	@RequestMapping("removeConta")
 	public String remove(Conta conta) {
 		
-		new ContaDAO().remove(conta);
+		dao.remove(conta);
 		System.out.println(conta.getId() + " removida com sucesso!");
 		//return "forward:listaContas";
 		return "redirect:listaContas";
@@ -62,7 +70,7 @@ public class FormularioContaController {
 	@RequestMapping("/listaContas")
 	public String listaTodos(Model mv) {
 		
-		List<Conta> listaContas = new ContaDAO().lista();
+		List<Conta> listaContas = dao.lista();
 		
 		System.out.println("Conta ---------------------"+ listaContas.size());
 		listaContas.forEach(System.out::println);
--- Treinamento_Spring/1 Spring MVC/projeto-spring/src/br/com/alexandre/contas/dao/ContaDAO.java
+++ Treinamento_Spring/1 Spring MVC/projeto-spring/src/br/com/alexandre/contas/dao/ContaDAO.java
@@ -9,18 +9,24 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import br.com.alexandre.contas.ConnectionFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import br.com.alexandre.contas.modelo.Conta;
 import br.com.alexandre.contas.modelo.TipoDaConta;
 
+@Repository
 public class ContaDAO {
 	private Connection connection;
-
-	public ContaDAO() {
+	
+	@Autowired
+	public ContaDAO(DataSource ds) {
 		try {
-			this.connection = new ConnectionFactory().getConnection();
+			this.connection = ds.getConnection();
 		} catch (SQLException e) {
-			throw new RuntimeException(e);
+			e.printStackTrace();
 		}
 	}
 

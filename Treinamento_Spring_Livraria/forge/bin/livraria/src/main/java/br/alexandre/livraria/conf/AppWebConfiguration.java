c8c71cca8bf3e00c97eeb312d4db27f158b45f47
Alexandre Ximenes <xyymenes@gmail.com>
2018-05-18 22:17:51 -0300

    Configuracoes iniciais do projeto

---
+++ Treinamento_Spring_Livraria/forge/bin/livraria/src/main/java/br/alexandre/livraria/conf/AppWebConfiguration.java
@@ -0,0 +1,22 @@
+package br.alexandre.livraria.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.alexandre.livraria.controller.HomeController;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses= {HomeController.class})
+public class AppWebConfiguration {
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		
+		return resolver;
+	}
+}

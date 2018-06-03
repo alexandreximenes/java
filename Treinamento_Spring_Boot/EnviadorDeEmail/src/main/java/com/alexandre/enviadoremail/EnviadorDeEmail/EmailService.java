658d8d7d99a98af17b528105b6a85e5d0b6aa8ed
Alexandre Ximenes <xyymenes@gmail.com>
2018-06-03 17:46:07 -0300

    Criando um novo projeto para serviço de envio de email

---
+++ Treinamento_Spring_Boot/EnviadorDeEmail/src/main/java/com/alexandre/enviadoremail/EnviadorDeEmail/EmailService.java
@@ -0,0 +1,8 @@
+package com.alexandre.enviadoremail.EnviadorDeEmail;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+}
---
+++ Treinamento_Spring_Boot/EnviadorDeEmail/src/main/java/com/alexandre/enviadoremail/EnviadorDeEmail/EnviadorDeEmailApplication.java
@@ -0,0 +1,12 @@
+package com.alexandre.enviadoremail.EnviadorDeEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviadorDeEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviadorDeEmailApplication.class, args);
+	}
+}

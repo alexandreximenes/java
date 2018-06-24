7a7f90b95cc02ca8dc42314e963d26a158db755c
Alexandre Ximenes <xyymenes@gmail.com>
2018-06-24 16:24:25 -0300

    configurando projeto

---
+++ maratona-jsf/src/main/java/bean/estudante/EstudanteRegistrarBean.java
@@ -0,0 +1,54 @@
+package bean.estudante;
+
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+public class EstudanteRegistrarBean implements Serializable {
+
+    private String nome = "Alexandre";
+    private String sobrenome = "Ximenes";
+    private double nota1 = 10;
+    private double nota2 = 20;
+    private double nota3 = 30;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+}

76e443e6d95824b22c43a5e9f3386cacf7951237
Alexandre Ximenes <xyymenes@gmail.com>
2018-06-27 21:12:07 -0300

    configurando o jpa e hibernate

--- Treinamento_spring_boot_rest/src/main/java/br/com/treinamento/springboot/demo/model/Student.java
+++ Treinamento_spring_boot_rest/src/main/java/br/com/treinamento/springboot/demo/model/Student.java
@@ -3,12 +3,14 @@ package br.com.treinamento.springboot.demo.model;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
 public class Student extends AbstractEntity {
-
     private String nome;
 
     public String getNome() {

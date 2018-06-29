9fe51832032b6c087b75d996fa11ec06e1932491
Alexandre Ximenes <xyymenes@gmail.com>
2018-06-28 21:25:36 -0300

    Construindo busca personalizada com Spring Data

--- Treinamento_spring_boot_rest/src/main/java/br/com/treinamento/springboot/demo/repository/StudentRepository.java
+++ Treinamento_spring_boot_rest/src/main/java/br/com/treinamento/springboot/demo/repository/StudentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
-    //List<Student> findByNameIgnoreCaseContaining(String nome);
+    List<Student> findByNomeIgnoreCaseContaining(String nome);
 
 
 

2d67eb5d59371de13c6d6adf3ffab9dd27fefb15
Alexandre Ximenes <xyymenes@gmail.com>
2018-08-29 18:26:26 -0300

    criando um novo projeto com spring boot initializer

---
+++ Treinamento_Spring_Boot/evento/src/main/java/controllers/EventoController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+
+	@RequestMapping("cadastrarEvento")
+	public String form() {
+		return "formEvento";
+	}
+}

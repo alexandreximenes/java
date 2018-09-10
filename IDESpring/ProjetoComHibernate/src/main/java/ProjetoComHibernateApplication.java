93dbd1da7a0867b46863d197134ed386a0f38ead
Alexandre Ximenes <xyymenes@gmail.com>
2018-09-09 23:01:55 -0300

    criando app com Hibernate 5

--- IDESpring/ProjetoComHibernate/src/main/java/ProjetoComHibernateApplication.java
+++ IDESpring/ProjetoComHibernate/src/main/java/ProjetoComHibernateApplication.java
@@ -3,17 +3,21 @@
 import java.math.BigDecimal;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.example.dao.EmployeRepository;
 import com.example.model.Employe;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.example.model", "com.example.dao", "com.example.config"})
 public class ProjetoComHibernateApplication implements CommandLineRunner{
 
-	private EmployeRepository employeRepository = new EmployeRepository();
+	@Autowired
+	private EmployeRepository employeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoComHibernateApplication.class, args);

b78ae58c04a48d3e4fb282ef1d1806f5867d582e
Alexandre Ximenes <xyymenes@gmail.com>
2018-09-17 22:20:46 -0300

    update
    Alterando nome do projeto

---
+++ Treinamento_JavaSE/src/Generics/ObjetosGenericos.java
@@ -0,0 +1,51 @@
+package Generics;
+
+import com.sun.org.apache.xpath.internal.operations.Number;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ObjetosGenericos<T> {
+
+    private T elemento;
+
+    public ObjetosGenericos() {
+
+    }
+    public ObjetosGenericos(T elemento){
+
+        this.elemento = elemento;
+    }
+
+
+    public T getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(T elemento) {
+        this.elemento = elemento;
+    }
+
+    /**
+     * Tornando metodo soma generico
+     * @param lista
+     * @return
+     */
+    public double soma(List<? extends Number> lista){
+        return lista.stream().mapToDouble(value -> ((Number) value).getColumnNumber()).sum();
+    }
+
+    public void exibeDadosOrdenados(List<? extends String> lista){
+        lista.sort(Comparator.comparing(l -> l.length()));
+    }
+
+    @Override
+    public String toString() {
+        return "ObjetosGenericos{" +
+                "elemento=" + elemento +
+                '}';
+    }
+}
+
+
---
+++ Treinamento_JavaSE/src/Generics/TestandoObjetoGenerico.java
@@ -0,0 +1,30 @@
+package Generics;
+
+import static java.util.Arrays.asList;
+
+public class TestandoObjetoGenerico {
+
+    public static void main(String[] args) {
+
+        ObjetosGenericos<String> objetoG = new ObjetosGenericos<>();
+        objetoG.setElemento("qualquer coisa que seja uma String neste momento");
+        //objetoG.setElemento(123); // not compile
+
+
+        System.out.println(
+                objetoG
+                    .getElemento()
+                    .toUpperCase()
+                    .toString()
+        );
+
+        //Testando tipo
+        System.out.println(objetoG.getElemento() instanceof String);
+
+//        double soma = objetoG.soma(asList(10, 10, 20, 10, 1.2));
+
+
+        //System.out.println(objetoG.exibeDadosOrdenados(asList("c", "d", "a")));
+    }
+
+}
